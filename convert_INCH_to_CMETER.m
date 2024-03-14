099aac34da461349e013c9f014c807cb35259ced
soheil <140661028+soheilsolim@users.noreply.github.com>
2024-03-14 10:51:59 -0700

    Add files via upload

---
+++ convert_INCH_to_CMETER.m
@@ -0,0 +1,7 @@
+%soheil mansouri (electrical engineering)
+%Script file to convert inch to cmeter
+%
+INCH=input('number in INCH:');  %give INCH
+CMETER=(INCH*2.54);
+disp(['number in CMETER=',num2str(CMETER)])  %get CMETER
+%stop
